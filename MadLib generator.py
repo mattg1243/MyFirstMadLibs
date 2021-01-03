64658b9cb40f482ce912b120373818d0cb24dfc6
mattg1243 <76641882+mattg1243@users.noreply.github.com>
2021-01-03 00:34:36 -0800

    Add files via upload

---
+++ MadLib generator.py
@@ -0,0 +1,50 @@
+#input variables
+
+
+two_digit_number_1 = input("Enter a 2 digit number: ")
+noun_1 = input("Enter a plural noun: ")
+two_digit_number_2 = input("Enter a 2 digit number: ")
+noun_2 = input("Enter a plural noun: " )
+adj_1 = input("Enter an adjective: ")
+noun_3 = input("Enter a noun: " )
+year = input("Enter a year: " )
+adj_2 = input("Enter an adjective: " )
+noun_4 = input("Enter a noun: " )
+two_digit_number_3 = input("Enter a 2 digit number: ")
+noun_5 = input("Enter a noun: " )
+two_digit_number_4 = input("Enter a 2 digit number: ")
+adj_3 = input("Enter an adjective: " )
+noun_6 = input("Enter a noun: " )
+adj_4 = input("Enter an adjective: " )
+noun_7 = input("Enter a noun: " )
+noun_8 = input("Enter a noun: " )
+two_digit_number_5 = input("Enter a 2 digit number: " )
+noun_9 = input("Enter a noun: " )
+two_digit_number_6 = input("Enter a 2 digit number: " )
+verb_1 = input("Enter a verb: " )
+verb_2 = input("Enter a verb: " )
+adj_5 = input("Enter an adjective: " )
+noun_10 = input("Enter a noun: " )
+gerund = input("Enter a gerund: " )
+adj_6 = input("Enter an adjective: " )
+last_name = input("Enter an last name: " )
+adj_7 = input("Enter a mean adjective: " )
+noun_11 = input("Enter a dirty noun: " )
+past_tense_verb = input("Enter an aggressive past tense verb: " )
+noun_12 = input("Enter a noun: " )
+
+
+#MadLib print
+
+
+print(two_digit_number_1 + "% of internet " + noun_1 + ", or about " + two_digit_number_2 + " million " + noun_2 +
+      ", said they went " + adj_1 + " to get news or " + noun_3 + " about the " + year + " elections.")
+print("We call them " + adj_2 + " political " + noun_4 + " consumers.")
+print(two_digit_number_3 + "% of " + noun_5 + " users, or about " + two_digit_number_4 + " million people, said they used "+ adj_3 +
+      " to discuss " + noun_6 + ", and one of the most " + adj_4 + " " + noun_7 + " was jokes about the " + noun_8 + " and the election.")
+print(two_digit_number_5 + "% of " + noun_9 + ", or more than " + two_digit_number_6 + " million people, " + verb_1 + " online to " + verb_2 +
+      " directly in " + adj_5 + " activities such as donating " + noun_10 + ", volunteering, or " + gerund + " about " + adj_6 + " events to attend.")
+print("I think President " + last_name + " is a " + adj_7 + " " + noun_11 + " " + past_tense_verb + " in the " + noun_12 + ".")
+
+
+#MadLib template source - https://i.pinimg.com/originals/b2/ef/1a/b2ef1a52bdf630783945ed01c6d72b41.jpg
